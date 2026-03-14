1b55f0fcb54be6f1525a291f94891218faf5ad8a
HOTSONHONET <hotsonhonet@github.com>
2026-03-14 13:16:50 +0000

    doc: update Readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# CPP - AutoDiff
+
+The repository is a simple implementation of how to perform automatic differentiation for neural network training.
+
+
+### How to run
+
+- Make sure if you have `g++` installed
+- Open the terminal at the root of the repository, and execute this command
+
+```
+
+g++ -std=c++17 main.cpp -O2 -o train && ./train
+
+```
--- nn.hpp
+++ nn.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "value.hpp"
 
+
+inline std::mt19937 rng(42);
+inline std::uniform_real_distribution<double> dist(-1.0, 1.0);
 struct Neuron {
     vector<Value*> w;
     Value* b;
 
     Neuron(int nin) {
-        std::mt19937 rng(42);
-        std::uniform_real_distribution<double> dist(-1.0, 1.0);
         for(int i = 0; i < nin; i++) {
             w.push_back(Value::create(dist(rng)));
         }
@@ -32,8 +33,9 @@ struct Neuron {
 
 struct Linear {
     vector<Neuron> neurons;
+    bool use_relu;
 
-    Linear(int nin, int nout) {
+    Linear(int nin, int nout, bool use_relu = true): use_relu(use_relu) {
         for(int i = 0; i < nout; i++) {
             neurons.emplace_back(nin);
         }
@@ -41,7 +43,9 @@ struct Linear {
 
     vector<Value*> operator()(const vector<Value*>&x) {
         vector<Value*> out;
-        for(auto &n: neurons) out.push_back(n(x));
+        for(auto &n: neurons){
+            out.push_back(use_relu ? relu(n(x)) : n(x));
+        }
         return out;
     }
 
@@ -60,17 +64,16 @@ struct MLP {
 
     MLP(int nin, const vector<int>& nouts) {
         int in = nin;
-        for(int nout: nouts) {
-            layers.emplace_back(in, nout);
-            in = nout;
+        for(int i = 0; i < (int)nouts.size(); i++) {
+            bool use_relu = (i != (int)nouts.size() - 1);
+            layers.emplace_back(in, nouts[i], use_relu);
+            in = nouts[i];
         }
     }
 
     vector<Value*> operator()(const vector<Value*> &x) {
         vector<Value*> out = x;
-        for(int i = 0; i < (int) layers.size(); i++) {
-            out = layers[i](out);
-        }
+        for(auto &l: layers) out = l(out);
         return out;
     }
 
